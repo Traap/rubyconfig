69673a2effd46b57e0d35a0cf306002eaa22e9cb
Traap <gary.a.howard@mac.com>
2021-01-02 20:37:02 -0600

    Pretty print output.

--- rbconfig.rb
+++ rbconfig.rb
@@ -1,9 +1,9 @@
 puts "arch = #{RbConfig::CONFIG['arch']}"
-puts "host =#{RbConfig::CONFIG['host']}"
+puts "host = #{RbConfig::CONFIG['host']}"
 puts "host_alias =#{RbConfig::CONFIG['host_alias']}"
-puts "host_cpu =#{RbConfig::CONFIG['host_cpu']}"
-puts "host_os =#{RbConfig::CONFIG['host_os']}"
-puts "host_vendor =#{RbConfig::CONFIG['host_vendor']}"
+puts "host_cpu = #{RbConfig::CONFIG['host_cpu']}"
+puts "host_os = #{RbConfig::CONFIG['host_os']}"
+puts "host_vendor = #{RbConfig::CONFIG['host_vendor']}"
 puts "\n----------------------------------------\n"
 RbConfig::CONFIG.sort.each do |key, value|
    puts "#{key} = #{value}"
